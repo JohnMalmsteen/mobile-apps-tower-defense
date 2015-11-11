e2e9c022e87b030967b75de54576eab20960bd56
JohnMalmsteen <g00305339@gmit.ie>
2015-11-11 18:25:36 +0000

    Added a couple more spells

---
+++ Assets/scripts/Spells/CureLightWounds.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CureLightWounds : Spell {
+	public CureLightWounds()
+	{
+		range = 0;
+		damage = 8;
+		spellType = (int)SpellTypes.Healing;
+		damageMultiplier = 1;
+		duration = 0;
+		savingThrow = (int)SavingThrow.None;
+		spellLevel = 1;
+	}
+
+	public override int cast(Unit target, int autoFailChance)
+	{
+		int healthBuff = (int)Random.Range(1, damage+1);
+		healthBuff += damageMultiplier;
+		if((int)Random.Range(0, 101) > autoFailChance)
+		{
+			return 0;
+		}
+		else
+		{
+			target.health = ((target.health + healthBuff) > target.maxHealth) ? target.maxHealth : target.health + healthBuff;
+			return healthBuff;
+		}
+	}
+}
---
+++ Assets/scripts/Spells/CureMinorWounds.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CureMinorWounds : Spell {
+	public CureMinorWounds()
+	{
+		range = 0;
+		damage = 1;
+		spellType = (int)SpellTypes.Healing;
+		damageMultiplier = 1;
+		duration = 0;
+		savingThrow = (int)SavingThrow.None;
+		spellLevel = 0;
+	}
+	
+	public override int cast(Unit target, int autoFailChance)
+	{
+		int healthBuff = 1;
+		if((int)Random.Range(0, 101) > autoFailChance)
+		{
+			return 0;
+		}
+		else
+		{
+			target.health = ((target.health + healthBuff) > target.maxHealth) ? target.maxHealth : target.health + healthBuff;
+			return healthBuff;
+		}
+	}
+}
