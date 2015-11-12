a6b6031987f512fcc279a83c5d276d329e15f857
JohnMalmsteen <g00305339@gmit.ie>
2015-11-12 12:01:37 +0000

    Continued work on the turn controller

--- Assets/scripts/TurnController.cs
+++ Assets/scripts/TurnController.cs
@@ -8,6 +8,7 @@ public class TurnController : MonoBehaviour {
 	public static attachableUnitDetails nebinsTower;
 	public static int turnCount = 0;
 	public static bool levelOver = false;
+	public static bool playerWin = false;
 
 	public Queue<attachableUnitDetails> actionOrder =  new Queue<attachableUnitDetails>();
 
@@ -26,12 +27,57 @@ public class TurnController : MonoBehaviour {
 		List<attachableUnitDetails> combined = new List<attachableUnitDetails>(playerUnits);
 		combined.AddRange(compUnits);
 		combined.Sort();
+		combined[combined.FindLastIndex+1] = nebinsTower;
 		actionOrder = new Queue<attachableUnitDetails>(combined);
 
 	}
 
 	public int GameLoop(Queue<attachableUnitDetails> actionQueue)
 	{
+		while(!levelOver)
+		{
+			attachableUnitDetails current = actionQueue.Dequeue();
+			int result = Turn (current);
+			if(result == 1)
+			{
+				levelOver = true;
+				playerWin = true;
+			}
+			else if(result == 2)
+			{
+				levelOver = true;
+			}
+		}
+		return 0;
+	}
+
+	public int Turn(attachableUnitDetails currentUnit)
+	{
+		/*
+		 * Move code goes here
+		 * if unit owner == 0
+		 * 		Present user with option to move the unit
+		 * else
+		 * 		move the computer unit
+		 * 
+		 * The player can also use this action to switch weapon 
+		 */
+
+		/*
+		 * Attack code goes here
+		 * If the player is in combat or has a ranged attack that goes here
+		 *
+		 */
+
+		/*
+		 * If the player has destroyed the tower 
+		 * 		return 1;
+		 * else if the player has no units left
+		 * 		return 2;
+		 * else
+		 * {
+		 */ 
+
 		return 0;
 	}
 }
