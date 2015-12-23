3521e3e40a43e2b098da45b70d40c4d9d2e2b464
AndyDev2013 <sweeneyandrew2013@gmail.com>
2015-12-23 00:17:41 +0000

    Added music singleton script, on the MUSIC object in the main scene

---
+++ Assets/scripts/Misc/MusicDontDie.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicDontDie : MonoBehaviour
+{
+
+    private static MusicDontDie instance = null;
+
+    public static MusicDontDie Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
